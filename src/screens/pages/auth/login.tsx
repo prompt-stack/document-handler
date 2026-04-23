0bd7a7e410827c76760e1ff653a2a1e1fb2399c3
realynpatoy <realynpatoy@gmail.com>
2026-04-23 20:49:35 +0800

    first commit

--- src/screens/pages/auth/login.tsx
+++ src/screens/pages/auth/login.tsx
@@ -1,7 +1,10 @@
+import ReusableInput from "../../components/ReusableInput";
+
 const Login = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-[70%] w-[50%] rounded bg-gray-100">
+        <div className="flex flex-col items-center justify-center h-[70%] w-[50%] rounded bg-gray-200">
             <h1 className="text-3xl font-bold text-red-500">Login Page</h1>
+            <ReusableInput/>
 
         </div>
     );
